48e9c5842069e0da1314710abe1b60fa2a601e15
Derek Steinmoeller <dsteinmo@gmail.com>
2017-04-02 20:15:01 -0400

    Param tweak in csanady_surfacemodes test case. Include some calculated frequencies for different params.

--- csanady_surfacemodes/csanady_surfacemodes.m
+++ csanady_surfacemodes/csanady_surfacemodes.m
@@ -14,8 +14,8 @@ USEMEANDEPTH = false; %use mean depth instead of full bathymetry?
 f = 2*(2*pi/(3600*24))*sin(43.7*pi/180); %43.7 deg. lattitude, (for model great lake, From Csanady 1967)
 g = 9.81;
 
-numpot = 100;  %number of potential basis functions
-numstrm = 100; %number of streamfunction basis functions
+numpot = 150;  %number of potential basis functions
+numstrm = 150; %number of streamfunction basis functions
 
 DUMP_TO_FILE = false;
 
@@ -31,3 +31,14 @@ depthdata.H = 75.0*ones(Ny, Nx);
 %-------------------------------------------------------------------
 
 basinmodes_curved;
+
+% Analytic n=1,m=0 mode: -6.96 (Csanday 1967)
+% Analytic n=1,m=0 mode:  7.80
+
+% Num_H_REFINES = 0 (for next 3).
+% N=1, numpot=150, numstrm=150. get: sigma_1/f = 6.9784, sigma_2/f = 7.8113 (very close to analytics)
+% N=2, numpot=150, numstrm=150. get: sigma_1/f = 6.9671, sigma_2/f = 7.8035 (even better!)
+% N=3, numpot=150, numstrm=150. get: sigma_1/f = 6.9668, sigma_2/f = 7.8038
+
+%NUM_H_REFINES = 1
+% N=2, numpot=150, numstrm=150. get: sigma_1/f = 6.9668, sigma_2/f = 7.8038
